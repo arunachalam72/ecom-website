d8b2f533cfb23970cc41d5cbee97cdac8c3c7a73
arun <161682482+arunachalam72@users.noreply.github.com>
2024-12-19 16:15:05 +0530

    Add files via upload

---
+++ ecommerceapp/views.py
@@ -0,0 +1,151 @@
+from django.shortcuts import render,redirect
+from .models import *
+import json
+from django.http import JsonResponse
+from django.contrib import messages
+from .forms import CustomUserForm
+from django.contrib.auth import authenticate,login,logout
+from django.core.paginator import Paginator
+# Create your views here.
+
+def home(request):
+    products=Product.objects.filter(trending=1)
+    paginator = Paginator(products, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request,'ecommerceapp/index.html',{'page_obj':page_obj})
+
+
+def favviewpage(request):
+    if request.user.is_authenticated:
+        fav=Favourite.objects.filter(user=request.user)
+        return render(request,'ecommerceapp/fav.html',{'fav':fav})
+    else:
+        return redirect('/')    
+
+
+def remove_fav(request,fid):
+    item=Favourite.objects.get(id=fid)
+    item.delete()
+    return redirect('/favviewpage')
+
+
+def cart_page(request):
+    if request.user.is_authenticated:
+        cart=Cart.objects.filter(user=request.user)
+        return render(request,'ecommerceapp/cart.html',{'cart':cart})
+    else:
+        return redirect('/')
+
+
+def remove_cart(request,cid):
+    cartitem=Cart.objects.get(id=cid)
+    cartitem.delete()
+    return redirect('/cart')
+
+def fav_page(request):
+   if request.headers.get('x-requested-with')=='XMLHttpRequest':
+    if request.user.is_authenticated:
+      data=json.load(request)
+      product_id=data['pid']
+      product_status=Product.objects.get(id=product_id)
+      if product_status:
+         if Favourite.objects.filter(user=request.user.id,product_id=product_id):
+          return JsonResponse({'status':'Product Already in Favourite'}, status=200)
+         else:
+          Favourite.objects.create(user=request.user,product_id=product_id)
+          return JsonResponse({'status':'Product Added to Favourite'}, status=200)
+    else:
+      return JsonResponse({'status':'Login to Add Favourite'}, status=200)
+   else:
+    return JsonResponse({'status':'Invalid Access'}, status=200)
+
+
+def add_to_cart(request):
+   if request.headers.get('x-requested-with')=='XMLHttpRequest':
+    if request.user.is_authenticated:
+      data=json.load(request)
+      product_qty=data['product_qty']
+      product_id=data['pid']
+      #print(request.user.id)
+      product_status=Product.objects.get(id=product_id)
+      if product_status:
+        if Cart.objects.filter(user=request.user.id,product_id=product_id):
+          return JsonResponse({'status':'Product Already in Cart'}, status=200)
+        else:
+          if product_status.quantity>=product_qty:
+            Cart.objects.create(user=request.user,product_id=product_id,product_qty=product_qty)
+            return JsonResponse({'status':'Product Added to Cart'}, status=200)
+          else:
+            return JsonResponse({'status':'Product Stock Not Available'}, status=200)
+    else:
+      return JsonResponse({'status':'Login to Add Cart'}, status=200)
+   else:
+    return JsonResponse({'status':'Invalid Access'}, status=200)
+
+    
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"Logged out Successfully")
+    return redirect('/')
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        if request.method=='POST':
+            name=request.POST.get('username')
+            pwd=request.POST.get('password')
+            user=authenticate(request,username=name,password=pwd)
+            if user is not None:
+                login(request,user)
+                messages.success(request,"Logged in Successfully")
+                return redirect("/")
+            else:
+                messages.error(request,"Invalid Username or Password")
+                return redirect("/login")
+        return render(request,'ecommerceapp/login.html')
+
+def register(request):
+    form=CustomUserForm()
+    if request.method=='POST':
+        form=CustomUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Registration success you can login now...!')
+            return redirect('/login')
+    return render(request,'ecommerceapp/register.html',{"form":form})
+
+def collections(request):
+    if request.user.is_authenticated:
+        catagory=Catagory.objects.filter(status=0)
+        return render(request,'ecommerceapp/collections.html',{'catagory':catagory})
+    else:
+        return redirect('/')
+
+def collectionsview(request,name):
+    if(Catagory.objects.filter(name=name,status=0)):
+        products=Product.objects.filter(category__name=name)
+        paginator = Paginator(products, 4)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        return render(request,'ecommerceapp/products/index.html',{'page_obj':page_obj,'category_name':name})
+    else:
+        messages.warning(request,"No such catagory found")
+        return redirect('collections')
+
+def product_details(request,cname,pname):
+    if request.user.is_authenticated:
+        if(Catagory.objects.filter(name=cname,status=0)):
+            if(Product.objects.filter(name=pname,status=0)):
+                products=Product.objects.filter(name=pname,status=0).first()
+                return render(request,'ecommerceapp/products/product_details.html',{"products":products})
+            else:
+                messages.error(request,"no such product not found")
+                return redirect("collections") 
+        else:
+            messages.error(request,"no such catagory found")
+            return redirect("collections")
+    else:
+        return redirect('/')
